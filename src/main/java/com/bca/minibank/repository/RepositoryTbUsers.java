874382fbe3c0e6355e79d602b055f99325d211be
monikasuryadarma1 <eunikemonikas@gmail.com>
2020-04-27 16:40:46 +0700

    Revert "Merge branch 'bmonik'"
    This reverts commit b043f1c71bd01684149a4396a6ac7922b019986b, reversing
    changes made to d20f1b60842fb4922fa55b9181c6694141df1fce.

--- src/main/java/com/bca/minibank/repository/RepositoryTbUsers.java
+++ src/main/java/com/bca/minibank/repository/RepositoryTbUsers.java
@@ -1,11 +1,5 @@
 package com.bca.minibank.repository;
 
-import java.util.List;
-import java.util.Optional;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,8 +8,4 @@ import com.bca.minibank.entity.TbUsers;
 @Repository
 public interface RepositoryTbUsers extends JpaRepository<TbUsers, Integer> {
 
-	List<TbUsers> findByStatusUserAndRole(String statusUser, String role);
-	List<TbUsers> findByStatusUserAndRoleAndUsernameContainingIgnoreCase(String statusUser, String role, String username);
-	
-	
 }
