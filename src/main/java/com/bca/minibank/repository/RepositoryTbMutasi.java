8b491f6f6f3766e4f6860df5be92ce021521fbbe
jokopurwanto <jokopurwanto1996@email.unikom.ac.id>
2020-05-08 19:00:37 +0700

    update query

--- src/main/java/com/bca/minibank/repository/RepositoryTbMutasi.java
+++ src/main/java/com/bca/minibank/repository/RepositoryTbMutasi.java
@@ -17,29 +17,6 @@ public interface RepositoryTbMutasi extends JpaRepository<TbMutasi, Integer>{
     @Query("SELECT a FROM TbMutasi a WHERE a.noRek= ?1 order by a.tglMutasi desc")
     List<TbMutasi> findBySemua(String noRek);
     
-//    @Query("SELECT a FROM TbMutasi a WHERE TRUNC(a.tglMutasi) = TO_DATE(?1) order by a.tglMutasi desc")
-//    List<TbMutasi> findByPeriodeDay(String startDate1);
-    
-    @Query("SELECT a FROM TbMutasi a WHERE TRUNC(a.tglMutasi) = TO_DATE(?1) order by a.tglMutasi desc")
-    List<TbMutasi> findByPeriodeDay(String startDate);
-    
-//    @Query("SELECT a FROM TbMutasi a WHERE a.noRek= ?1 AND TRUNC(a.tglMutasi) BETWEEN TO_DATE(?2) AND TO_DATE(?3) order by a.tglMutasi desc")
-//    List<TbMutasi> findByPeriode(String noRek, String startDate, String endtDate);
-    
-//    @Query("SELECT a FROM TbMutasi a WHERE a.noRek= ?1 AND a.jnsMutasi= ?2 AND TRUNC(a.tglMutasi) BETWEEN TO_DATE(?3) AND TO_DATE(?4) order by a.tglMutasi desc")
-//    List<TbMutasi> findByPeriode(String noRek, String jnsMutasi, String startDate, String endtDate);
-    
-//    filter by semua transaksi
-    @Query("SELECT a FROM TbMutasi a WHERE a.noRek= ?1 AND TRUNC(a.tglMutasi) BETWEEN TO_DATE(?2) AND TO_DATE(?3) order by a.tglMutasi desc")
-    List<TbMutasi> findByAllTransaksi(String noRek, String startDate, String endtDate);
-    
-//  filter by uang masuk dan uang keluar  
-    @Query("SELECT a FROM TbMutasi a "
-    		+ "WHERE a.noRek= ?1 AND a.jnsMutasi= ?2 AND TRUNC(a.tglMutasi) BETWEEN TO_DATE(?3) AND TO_DATE(?4) "
-    		+ "order by a.tglMutasi desc")
-    List<TbMutasi> findByFilterTransaksi(String noRek, String jnsMutasi, String startDate, String endtDate);
-    
-    
     @Query("SELECT b FROM TbTransaksi a, TbMutasi b WHERE (a.noRekTujuan= ?1 AND TRUNC(a.tglTransaksi) BETWEEN TO_DATE(?2) AND TO_DATE(?3)) AND (b.tbTransaksi = a.idTransaksi AND 'UANG MASUK' = b.jnsMutasi) order by a.tglTransaksi desc")
     List<TbMutasi> findByFilterTransaksiIn(String noRek, String startDate, String endDate);
     
